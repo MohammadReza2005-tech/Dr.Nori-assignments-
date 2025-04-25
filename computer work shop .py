98b6bef7aaf2c7a6ea04d6002881ab2ca807b277
Mohammad Reza Rajabi <Rajamohammadreza344@gmail.com>
2025-04-26 03:16:02 +0330

    Add files via upload

---
+++ computer work shop .py
@@ -0,0 +1,413 @@
+s = "foobar"
+print(s[3])
+print(len(s))
+w = "let's rock it"
+print(w)
+print(w[:])
+print(w[::-1])
+e = "we are going to check on methods in python"
+print(e[3:13:])
+print(e[3:13:2])
+print(e[3:13:])
+print(s[:])
+print(e[:])
+print(e[-3])
+print(e[-3:-4])
+print(e[-3::])
+print(e[-6::])
+l1 = [3, 4, 5, 6, 7, 8, 9, 10]
+print(l1[3:9:2])
+print(l1[-4:-1])
+print(l1[-4:0])
+print(l1[-4:])
+s = "mobster"
+s = s + "in newyork"
+print(s)
+print(l1 * 10)
+print(s * 3)
+L = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(L)
+print(L[::-1])
+print(L[::-2])
+z = "friend"
+z = "good " + z
+print(z)
+d = "        today we are gonig to attend 3 classes in uni    "
+print(z.split())
+print(d.split())
+print(d.split('3 '))
+print(d.strip())
+print(d.rstrip())
+print(d.lstrip())
+s = "frog"
+print(s)
+print(s.upper())
+print(s[2].upper)
+L2 = "hello my darling how are you doing today"
+print(L2[3])
+print(s[2].upper())
+print(L2[4].upper())
+print(L2[10].split())
+print(L2[10].upper())
+print(L2.split()[3].upper())
+L3 = "good after noon sir"
+print(L3.split()[3].upper())
+print(L3.split()[1].upper())
+L4 = "back off bitch"
+print(L4.split()[2].upper())
+print(L4.split()[0].upper())
+print(L4[6].upper())
+L5 = "we are going to deteermine all codes at once"
+print(L5.capitalize())
+print(L5.title())
+L6 = "good Mornoing"
+print(L6.capitalize())
+print(L6.title())
+print(L6.split()[1].replace('Night', 'Morning')) 
+print(L6.split()[1])
+print(L6.split()[0])
+print(L6.replace('good', 'filter'))
+p = "wwwwwwwwwwwwwwwwww              wwwwwwwww       "
+print(p.split())
+print(p.strip())
+s4 = "wert" 
+s5 = s4 * 10
+print(s5)
+print(s5.split('e'))
+print(s5.split('w')[1].upper())
+print(s5.replace("e", "g"))
+print("i am going to %s today" %'mall')
+print("iam gong to have %s today but i am not %s about it"% ('fun', 'sure'))
+x, y, z = 'computer', 'workshop', 'fun'
+print("today in %s workshop i am going to %s and it is %s" %(x, y, z))
+print(f"today in {x} workshop i am going to {y} and it is {z}")
+print(L5) 
+my_age = [13]
+print(type(my_age))
+my_age2 = (13)
+print(type(my_age2))
+t = 'I\'m doing allright'
+print(t)
+u = [1, 2, 3, 4, 5]
+print(len(u))
+u1 = 'foobar'
+print(u1[-1])
+print(u1[-6:-1])
+print(u1[-6:])
+print(u1[::-1])
+u2 = [1, 2, 3, 4, 5, 6]
+print(u2[-3])
+print(u2[::-1])
+s = "ops"
+s = s + "you"
+print(s)
+letter = 'z'
+print(letter * 10)
+s = "hello world"[0:6]
+print(s)
+print(s.upper()[0:5])
+print(s.upper()[0:4])
+print(s.title()[0:4])
+print(s.upper()[0:4].replace('e', 'j'))
+print(s.upper()[0:4])
+d  = ("              r                   r     ")
+print(d.strip())
+print(d.lstrip())
+print(d.rstrip())
+print(d)
+print(1 + 2)
+print(2E6)
+print(6e-07)
+print(3e7)
+print(7e-02)
+print(7e-04)
+print(4 + 6)
+print(1 - 12)
+print(3 * 6) 
+e = 'R'
+print(e * 10)
+print(733/3)
+print(733//3)
+print(733%3)
+print(5%3)
+print(5**2)
+print(25**0.5)
+print(27**3.1)
+print(2+10*10+3)
+print((10+2)*10+3)
+print(10*5+10*10+6)
+a = 5 
+print(a)
+print(type(a))
+a2 = a**a 
+print(a2)
+my_income = 100
+tax_rate = 0.1
+my_taxes = my_income * tax_rate
+print(my_taxes)
+grade_a = 20 
+grade_b = 16
+ave = (grade_a + grade_b) / 2
+print(ave)
+a3 = (1, 2, 3, 4)
+print(type(a3))
+a4 = 14 
+print(type(a4))
+a5 = "we are going talk about data type in python"
+print(type(a5))
+s = 'foobar'
+print(s[0])
+print(s[-1:])
+print(s[0:])
+e1 = "rtrtrtrtrtrtrtrt"
+print(e1[1::2])
+e2 = "my friend is" 
+e2 = "my friend is" + " great man"
+print(e2)
+t1 = "hello you fool"
+print(t.upper())
+print(t.upper()[7])
+print(t)
+t = "NEW YORK THUG"
+print(t.lower())
+print(t.lower()[-1:])
+print(t.capitalize())
+print(t1.capitalize()[2])
+print(t1.count('o'))
+print(t1.count('11'))
+print(t.count('N'))
+print(t.replace('W', 'M'))
+N1 = "                this is what i am talking about                "
+print(N1.strip())
+print(N1.rstrip())
+n2 = "today i'm going to teach methods in python"
+print(n2.split())
+print(n2.split('g'))
+print("i'm going %say would you please %s eith me" %('zoo','come'))
+print("we are talking about %s and i'd like to %s about it more and more" %('network', 'explain'))
+print("we are talking about%r'd like to %rabout it more and more" %('network', 'explain'))
+d3 = "do you like this? \nyeah sure you ain't like it"
+print(d3)
+print("i'm going to write %s today" %('program'))
+print( " i got %s in math" %(19.75))
+print(" i got %d in matj" %(19.75))
+print(' i want to try this number : %1.13f' % 12.43565)
+print(" i have to visit this number today : %4.6f" % 345.45678910)
+print("floating point numbers: %2.16f" % 34.5678)
+print(' first:%5.5f, second:%s, third: %r' %(52.32,'good bye', 'why'))
+print('this is an string with{}'. format('trouble'))
+print(" i don't know why i have to study at {}". format('3.19AM'))
+print("we are going to {} in {}". format('method', 'python'))
+print('First object:{a}, second object:{b}, third object:{c}'. format(a = 'smoke',b = 23, c = (1, 2, 3, 4)))
+print('{0:8}/{1:8}'. format('orange','blue'))
+print('{0:8} / {1:8}'. format('Apple', '45'))
+print('{0:.30}/{1} / {2:8}'.format("I don't know why  I have to do this", str((1, 2, 3, 4, 5)), 34))
+print('{0:.30} / {1:8} / {2:8}'. format("I don't know why I have to do this", str((1, 2, 3, 4)), 456))
+print('{0:<8} / {1:^8} / {2:>8}'. format(11,12,13))
+print('{0:=<8} / {1:-^8} / {2:=^8}'. format(11,12,13))
+print('this is my ten-character, two-decimal number: %10.2f'%(13.579))
+print('this is my ten character, two-decimal number: (0:10.2f)'. format(13.579))
+print('this is going to be a 11 chaaracter, 4 decimal number: %11.5f' %(123.345))
+print("it's a new number : %5.6f" %(78.23))
+print("{0:-<8} , {1:=^8} , {2:>8}". format(11 , 12, 13))
+name = "Fred"
+print(f"he said his name is {name}")
+grade = 19 
+print(f"he scored {grade} in math exam")
+num = 23.45678
+print("My 10 character, four decimal number is : {0:10.4f}". format(num))
+num = 23.45678
+print("my 10 character, four decimal number is : {0:10.4f}". format(num))
+num2 = 12345.457987654321
+print("{0:12.3f}". format(num2))
+print("{0:10.3f}". format(num))
+from string import Template
+t = Template('Hey , $name')
+s = "johan"
+print(t.substitute( name = s))
+e = Template("Don't forget, $gold")
+r = "Never give up"
+print(e.substitute(gold= r))
+#List
+my_list = [1, 2, 3]
+print(my_list)
+print(len(my_list))
+my_list = ['foo', 'bar','baz','qux','quux','corge']
+print(my_list[0])
+print(my_list[0].upper()[1])
+print(my_list[1].upper()[0])
+print(my_list[2].upper())
+print(my_list[1:])
+print(my_list[2:6].append('Hello'))
+print(my_list[2:6].insert(23, 24))
+print(my_list[:4])
+print(my_list[::-1])
+print('qux'in my_list)
+print('bee'in my_list)
+print('bob'not in my_list)
+my_list[1] = 35
+print(my_list)
+my_list = my_list + ["this is what I'm talking about"]
+print(my_list)
+my_list[-2] = 86
+print(my_list)
+print(id(my_list))
+print(id(num2))
+print(id(1))
+l = [1, 2, 3]
+l1 = 1
+l2 = [3,2,1]
+print(l is l1)
+print(l1 is l2)
+print(l == l1)
+print(my_list + ['new item'])
+my_list = my_list + ['new item']
+my_list = my_list + [5]
+print(my_list * 3)
+l.append(-100)
+print(l)
+l2.append(66)
+print(l2)
+l2.extend([555,585,569])
+print(l2)
+print(a)
+print(my_list)
+my_list += ['bbb', 'sss']
+
+print(my_list)
+my_list += ['new item']
+print(my_list)
+print(my_list.pop(0))
+print(my_list)
+print(my_list.pop(2)) 
+print(my_list)
+list1 = [1,2,3,4,5,6,7]
+list1.remove(1)                   # Removes the first occurrence of 1
+print(list1)
+list1 = list1[4:]
+print(list1)
+list1 = [1,2,3,4,5,6,7]
+del list1[5]                      # it's easier method to use 
+print(list1)
+del list1[0:3]                    # we can use del method way better than remove method
+print(list1)
+# we can use "sort" & "reverse" to changr lists
+new_list = ['a', 'e','x', 'b','c']
+print(new_list)
+new_list.reverse()
+print(new_list)                     #we'd better use method "remove" in seprated lines 
+new_list.sort()
+print(new_list)
+new_list.sort(reverse=True)
+print(new_list)
+l = new_list.copy()
+print(l)
+print(l is new_list)
+a = ['foo', 'bar', 'baz', 'qux', 'quux', 'corge']
+print(a[0:4])
+print(a[1:4])
+a[1:4] = [11, 22, 33]                # here is how we can change some indexes 
+print(a)
+a[0:2] = [-100, 200, -300, -400]      # we you run this code 22 well be there 
+print(a)
+a[0:2] = ['see', 'bad']
+print(a)
+matrix = [["I'm just going to test it"], [9, 8, 7], [1, 2, 3]]
+print(matrix[1][0])
+print(matrix[-1])
+x = ['a', ['bb', ['ccc', 'ddd'], 'ee', 'ff'], 'g', ['hh', 'ii'], 'j']
+print(x[1])
+print(x[1][1])
+print(x[1][2][0])
+print(x[1][1][1].upper())
+print(x[2].upper())
+# here we are going to talk about tuple
+t = (1,2,3,4,5,6)
+print(type(t))   # it is how we check type 
+print(len(t))
+t = t + ("one ", "fight")
+print(t)
+print(t[-1].upper())
+x = (1)
+print(x)
+print(type(x)) # the out put refers that it's int
+x = (1,)
+print(type(x)) # here the type is tuple
+print(x)
+print(t)
+print(t.index(2))
+print(t)
+print(t.count('one '))
+t = (1,2,3,(5,6),[10,11,12])
+print(t)
+print(t[3][1])
+t[4][1] = -269
+print(t)
+# here is how we ca pack or unpack tuple
+x = 1,12,'best server ever', 'Travis Scott- Ikonw is great'
+print(x)
+print(type(x))
+# TUPLE UNPACKING
+x1,x2,x3,x4 = x
+print(x1,x2,x3,x4)
+# there are 2 methods for tuple : count , index 
+# next we are going to talk about Dictionaries
+MLB_team = {'Colorado': 'Rockies','Boston' : 'Red Sox','Minnesota' : 'Twins','Milwaukee' : 'Brewers','Seattle' : 'Mariners'}
+print(MLB_team['Colorado'])   # this is how we can access a key 
+print(MLB_team['Minnesota'])  
+MLB_team['Seattle'] = 'Seahawks'
+print(MLB_team)
+my_dict = {'key1':123, 'ke2':[12,23,33],'key3': ['item','item1', 'item2']}
+print(my_dict['key3'])
+print(my_dict['ke2'][1])
+print(my_dict['key3'][2].upper())
+d = {(-1,-4):'Tabriz', (0,0): 'Tehran', (0,-3): 'Shiraz'}
+print(d)
+print(d[(0,-3)])
+d ={} # we can create dictionaries like this
+d['animal'] = 'Dog'
+d['answer'] = 42
+print(d)                            # answer will be {'animal': 'Dog', 'answer': 42}
+d = {'key':{'nestkey':{'subnestkey': 'value'}}}
+print(d['key']['nestkey']['subnestkey'])
+print(d['key']['nestkey']['subnestkey'].upper())
+# Let's check some methods in dictionaries
+D = {'key1': 1 ,'key2': 2,'key3':3}
+print(D.keys())
+print(D.values())
+print(D.items())
+print(D.get('key1'))
+print(D.pop('key1'))
+print(D)
+print(D.popitem())
+d1 = {'a': 1 , 'b': 5}
+d2 = {'b':777 , 'c': -15}
+d1.update(d2)
+print(d1)
+x = set()
+print(type(x))
+x.add(1)
+print(x)
+x.add(2)
+print(x)
+print(1 in x)
+print(3 in x)
+x = {1,2,3,4,5,6,7}
+y = {4,6,10,11,12}
+print(x.intersection(y))
+print(x.union(y))
+print(x.difference(y))
+print(x.symmetric_difference(y))
+a = True
+print(type(a))
+print( 1>2)
+print(10*2+3 > 10*2)
+print(23*4-41 <3*12+18)
+
+
+
+
+
+
+
+
